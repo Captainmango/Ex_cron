9c24cbe09e164993f5c2cfcc0a4f6ed4639aee11
Edward Heaver <edward.heaver01@gmail.com>
2024-10-22 19:27:24 +0100

    Using byte sequences to match. Need way more tests and to work out what structures I'm passing around

--- lib/parser/rules.ex
+++ lib/parser/rules.ex
@@ -1,5 +1,9 @@
 defmodule Parser.Rules do
-  def evaluate(_) do
+  def evaluate(<<_::binary-size(1), "-", _::binary>>) do
+    Common.RulesTypes.range
+  end
+
+  def evaluate(<<"*">>) do
     Common.RulesTypes.wildcard
   end
 end
