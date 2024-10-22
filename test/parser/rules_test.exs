d7c6b52cab1cc6ffa99ac1b36629b52f01fb3747
Edward Heaver <edward.heaver01@gmail.com>
2024-10-22 19:33:46 +0100

    function rename. ElixirLS doesn't have rename symbol :(

--- test/parser/rules_test.exs
+++ test/parser/rules_test.exs
@@ -2,22 +2,22 @@ defmodule Parser.RulesTest do
   use ExUnit.Case
 
   test "It can parse a wild card argument" do
-    assert Parser.Rules.evaluate("*") == Common.RulesTypes.wildcard
+    assert Parser.Rules.evaluate_rule("*") == Common.RulesTypes.wildcard
   end
 
   test "It can parse a range argument" do
-    assert Parser.Rules.evaluate("1-5") == Common.RulesTypes.range
+    assert Parser.Rules.evaluate_rule("1-5") == Common.RulesTypes.range
   end
 
   test "It can parse a div mod argument" do
-    assert Parser.Rules.evaluate("*/5") == Common.RulesTypes.divisor
+    assert Parser.Rules.evaluate_rule("*/5") == Common.RulesTypes.divisor
   end
 
   test "It can parse a singular number argument" do
-    assert Parser.Rules.evaluate("1") == Common.RulesTypes.single_num
+    assert Parser.Rules.evaluate_rule("1") == Common.RulesTypes.single_num
   end
 
   test "It can parse a list argument" do
-    assert Parser.Rules.evaluate("1,5") == Common.RulesTypes.list
+    assert Parser.Rules.evaluate_rule("1,5") == Common.RulesTypes.list
   end
 end
