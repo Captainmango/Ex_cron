4d9b12675e37295cccf977b081b39c0ea3ca7d33
Edward Heaver <edward.heaver01@gmail.com>
2024-10-23 21:32:18 +0100

    parameterised test in

--- test/parser/tokeniser_test.exs
+++ test/parser/tokeniser_test.exs
@@ -1,8 +1,17 @@
 defmodule Parser.RulesTest do
   use ExUnit.Case
 
-  test "It can parse wildcard" do
-    assert Parser.Tokeniser.run("*") == {Parser.RuleTypes.wildcard}
+  for {tc, input, result} <- [
+    {"wildcard", "*", {Parser.RuleTypes.wildcard}},
+    {"range", "1-5", {Parser.RuleTypes.range, 1, 5}}
+  ] do
+    @description tc
+    @expected input
+    @actual result
+
+    test "Can handle #{@description} with input #{@expected}" do
+      assert Parser.Tokeniser.run("#{@expected}") == @actual
+    end
   end
 
   test "It raises an error if the input is incorrect" do
